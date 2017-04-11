926ddbf8dd7b2e62c85ef14654d9a63d89a9b0fb
Hack Reactor Student <student@hackreactor.com>
2017-04-10 20:34:09 -0700

    first tests passed in SongModel

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,9 +1,28 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
-var SongQueue = Backbone.Collection.extend({
+// var SongQueue = Backbone.Collection.extend({
 
-  model: SongModel,
+//   model: SongModel,
 
-  initialize: function() {
-  }
+//   initialize: function() {
+//   //enqueue - add - 
+//   //dequeue - remove 
+//   // //add or remove event,  render the view
+//   // this.model.on('enqueue', this.render, this);
+//   // this.model.on('dequeue', this.render, this);
+//   //   console.log(this);
+//   //   this.on('add', function(){this.playFirst();} );
+//   // // this.collection.forEach(function(songModel){
+//   // // 	this.playFirst(songModel);
+//   // // });
+//   // },
+//   // // playFirst: function(song){
+//   // //   if(this.collection.length === 1){
+//   // // 		song.play();
+//   // // 	}
+//   // // },
+//   // add : function(song){
 
-});
+//    }
+
+  
+// });
